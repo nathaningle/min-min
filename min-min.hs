de94174a7068decd5df54529aebd88111a033ef9
Nathan Ingle <elgni.nahtan@gmail.com>
2021-05-29 06:20:17 +1000

    Initial commit

---
+++ min-min.hs
@@ -0,0 +1,16 @@
+{- |
+File        : min-min.hs
+Copyright   : (c) 2021 Nathan Ingle
+Licence     : ISC
+
+Maintainer  : elgni.nahtan@gmail.com
+Stability   : experimental
+Portability : non-portable
+
+A minimal JavaScript minifier.
+-}
+import           Token
+
+
+main :: IO ()
+main = hello
---
+++ src/Token.hs
@@ -0,0 +1,15 @@
+{- |
+Module      : Token
+Copyright   : (c) 2021 Nathan Ingle
+Licence     : ISC
+
+Maintainer  : elgni.nahtan@gmail.com
+Stability   : experimental
+Portability : non-portable
+
+A minimal JavaScript minifier.
+-}
+module Token where
+
+hello :: IO ()
+hello = putStrLn "hello"
